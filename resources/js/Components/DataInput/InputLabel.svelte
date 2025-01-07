dd75a9f28682643bad307b7893b23e211da4f01b
EmilienKopp <emilien.kopp@gmail.com>
2025-01-07 23:08:45 +0100

    refactor: update InputLabel component to correctly bind htmlFor prop and support additional attributes

--- resources/js/Components/DataInput/InputLabel.svelte
+++ resources/js/Components/DataInput/InputLabel.svelte
@@ -1,7 +1,7 @@
 
 <script lang="ts">
-  let { children, htmlFor } = $props();
+  let { children, for: htmlFor, ...others } = $props();
 </script>
-<label class="block text-sm font-medium text-gray-700"> 
+<label class="block text-sm font-medium text-gray-700" for={htmlFor} {...others}> 
     {@render children?.()}
 </label>
