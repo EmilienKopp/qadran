b102d564bcf281fbdbc7ffe2c2f4e042f47938ca
EmilienKopp <emilien.kopp@gmail.com>
2025-01-19 02:56:20 +0900

    feat: implement soft deletes for projects model and add migration

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
 
 class Project extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectFactory> */
-    use HasFactory, Searchable;
+    use HasFactory, Searchable, SoftDeletes;
 
     protected $guarded = [];
 
