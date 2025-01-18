7fdead9cbea279e5ca92add561eb54931f735354
EmilienKopp <emilien.kopp@gmail.com>
2025-01-19 02:58:26 +0900

    feat: add Clock component to display current time on Dashboard and handle timezones on the front

---
+++ resources/js/Components/UI/Clock.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+
+  let time = $state(dayjs().format('HH:mm:ss'));
+
+  $effect(() => {
+    const interval = setInterval(() => {
+      time = dayjs().format('HH:mm:ss');
+    }, 1000);
+
+    return () => clearInterval(interval);
+  })
+</script>
+
+{#if time}
+  <div class="text-2xl font-bold text-center">
+    {time}
+  </div>
+{/if}
--- resources/js/Pages/Dashboard.svelte
+++ resources/js/Pages/Dashboard.svelte
@@ -2,23 +2,33 @@
   import Button from '$components/Actions/Button.svelte';
   import Select from '$components/DataInput/Select.svelte';
   import EntriesList from '$components/Entries/EntriesList.svelte';
+  import Clock from '$components/UI/Clock.svelte';
   import AuthenticatedLayout from '$layouts/AuthenticatedLayout.svelte';
   import { ClockEntry } from '$lib/domain/ClockEntry';
+  import { User } from '$lib/domain/User/index.svelte';
   import { superUseForm } from '$lib/inertia';
   import { asSelectOptions } from '$lib/utils/formatting';
-  import type { User, Project } from '$models';
+  import { getTimezone } from '$lib/utils/timezone';
+  import type { Project } from '$models';
+  import dayjs from 'dayjs';
+
+  
 
   interface Props {
     user: User;
   }
 
   let { user }: Props = $props();
+  let latestEntry = $derived(user?.todays_entries?.[0]);
+
   let projectOptions = asSelectOptions<Project>(user.projects, 'id', 'name');
 
   let form = superUseForm({
-    project_id: undefined,
+    project_id: latestEntry?.project_id,
+    timezone: getTimezone(),
   });
-  
+
+  $inspect($form);
 </script>
 
 <svelte:head>
@@ -27,16 +37,22 @@
 
 <AuthenticatedLayout>
   {#snippet header()}
-    <h2  class="text-xl font-semibold leading-tight text-gray-800">
-      Dashboard
+    <h2 class="text-xl font-semibold leading-tight text-gray-800">
+      <Clock />
     </h2>
   {/snippet}
 
   <div class="py-12 w-full">
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 w-full">
       <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg w-full">
-        <div class="p-6 text-gray-900 flex flex-col items-center justify-between w-full">
-          <Select label="Select a project" bind:value={$form.project_id} options={projectOptions} />
+        <div
+          class="p-6 text-gray-900 flex flex-col items-center justify-between w-full"
+        >
+          <Select
+            label="Select a project"
+            bind:value={$form.project_id}
+            options={projectOptions}
+          />
           <Button onclick={() => ClockEntry.push($form)} class="mt-4">
             Submit
           </Button>
