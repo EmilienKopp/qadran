e280b80f071d813207dd36b5dab65a53fcb8f48d
EmilienKopp <emilien.kopp@gmail.com>
2025-01-19 02:57:36 +0900

    feat: add InertiaHelper utility for standardized validation error handling

---
+++ app/Utils/InertiaHelper.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Utils;
+
+use Illuminate\Validation\ValidationException;
+
+class InertiaHelper
+{
+    public static function fail(string $message, array $errors = [])
+    {
+      throw ValidationException::withMessages([
+        'error' => [$message],
+        ...$errors,
+      ]);
+    }
+}
