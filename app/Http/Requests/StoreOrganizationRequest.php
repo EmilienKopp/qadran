9b421ba996c4a598332fe35df84f7626dfe469bd
EmilienKopp <emilien.kopp@gmail.com>
2025-01-22 01:33:44 +0900

    feat: update StoreOrganizationRequest to authorize all requests and add validation rules for name, type, and description

--- app/Http/Requests/StoreOrganizationRequest.php
+++ app/Http/Requests/StoreOrganizationRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Enums\OrganizationType;
 
 class StoreOrganizationRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreOrganizationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,9 @@ class StoreOrganizationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'type' => ['required', 'string', 'in:' . OrganizationType::implode()],
+            'description' => ['nullable', 'string'],
         ];
     }
 }
