f7ff96c5912c39029cc73dd6dc6cbbc16d035c10
EmilienKopp <emilien.kopp@gmail.com>
2025-01-21 16:45:31 +0900

    feat: add user role retrieval functions to inertia library

--- resources/js/Lib/inertia/index.ts
+++ resources/js/Lib/inertia/index.ts
@@ -1,14 +1,38 @@
 import { useForm } from '@inertiajs/svelte';
-import { Readable } from 'svelte/store';
+import { get, Readable } from 'svelte/store';
 import type { InertiaForm } from './types';
+import { page } from '@inertiajs/svelte';
 
 export type * from './types';
 
-export function superUseForm<T extends object>(obj?: Partial<T>): Readable<InertiaForm<T>> {
+export function superUseForm<T extends object>(
+  obj?: Partial<T>
+): Readable<InertiaForm<T>> {
   return useForm(obj as any);
 }
 
-export function hookSuccess<T extends object>(form: InertiaForm<T>, callback: () => void): void {
+export function hookSuccess<T extends object>(
+  form: InertiaForm<T>,
+  callback: () => void
+): void {
   form.recentlySuccessful && callback();
 }
 
+/**
+ * @returns The first role of the user
+ */
+export function getUserRoleName(): string {
+  const role = get(page).props.auth.user.roles?.[0];
+  return role?.name ?? 'guest';
+}
+
+/**
+ * @returns Array of user roles except the default role
+ */
+export function getAllUserRoles(): string[] {
+  return (
+    get(page)
+      .props.auth.user.roles?.map((role: any) => role.name)
+      .filter((r) => r != 'user') ?? []
+  );
+}
