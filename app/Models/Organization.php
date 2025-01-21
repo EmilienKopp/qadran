156dec5e72eaece171d78ebbc1c065a467f7dc9d
EmilienKopp <emilien.kopp@gmail.com>
2025-01-21 16:43:35 +0900

    feat: update UpdateOrganizationRequest to authorize requests and define validation rules

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -10,6 +10,12 @@ class Organization extends Model
     /** @use HasFactory<\Database\Factories\OrganizationFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'type',
+        'description',
+    ];
+
     public function users()
     {
         return $this->belongsToMany(User::class)
