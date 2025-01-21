685b48afc136ff3e2d628de662329589f1dacc87
EmilienKopp <emilien.kopp@gmail.com>
2025-01-22 01:34:35 +0900

    feat: add RoleSwitcher component and globale role context

---
+++ resources/js/Components/UI/RoleSwitcher.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Select from '$components/DataInput/Select.svelte';
+  import { asSelectOptions } from '$lib/utils/formatting';
+  import { getAllUserRoles, getUserRoleName } from '$lib/inertia';
+  import { RoleContext } from '$lib/stores/global/roleContext.svelte';
+  import { page } from '@inertiajs/svelte';
+
+  RoleContext.selected = getUserRoleName();
+  RoleContext.available = getAllUserRoles();
+</script>
+
+<div class="form-control w-full max-w-xs">
+  <Select
+    name="role"
+    bind:value={RoleContext.selected}
+    options={asSelectOptions(RoleContext.available)}
+    class="select-primary select w-full bg-white shadow-sm transition-all hover:shadow-md"
+  />
+</div>
