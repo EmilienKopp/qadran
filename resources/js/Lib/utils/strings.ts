4fa6ab4889a2ed30f9efaa0148ec16617550451d
EmilienKopp <emilien.kopp@gmail.com>
2025-02-28 23:22:35 +0900

    feat: add caseInsensitiveIncludes utility function to string utilities

--- resources/js/Lib/utils/strings.ts
+++ resources/js/Lib/utils/strings.ts
@@ -1,3 +1,9 @@
 export function capitalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function caseInsensitiveIncludes(haystack?: string, needle?: string) {
+    if (!needle?.length) return true;
+    if(!haystack?.length) return false;
+    return haystack.toLowerCase().includes(needle.toLowerCase());
+}
