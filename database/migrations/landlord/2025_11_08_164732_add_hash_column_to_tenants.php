555dce55a9ae6d6a60c8e8c170be8a1c2aeccfb2
EmilienKopp <emilien.kopp@gmail.com>
2025-11-09 02:02:00 +0900

    feat(migration): Add hash and n8n_webhooks columns to tenants table

---
+++ database/migrations/landlord/2025_11_08_164732_add_hash_column_to_tenants.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            $table->string('hash')->nullable();
+            $table->jsonb('n8n_webhooks')->nullable();
+        });
+
+        $tenants = \App\Models\Landlord\Tenant::all();
+        foreach ($tenants as $tenant) {
+            $hash = hash('sha256', $tenant->id . env('APP_KEY'));
+            $tenant->hash = $hash;
+            $tenant->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            $table->dropColumn('hash');
+        });
+    }
+};
